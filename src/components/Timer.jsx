e43a2fd2351bf71361fd35e16706602e85ecb6eb
yitzhak-bloy <41252020+yitzhak-bloy@users.noreply.github.com>
2020-01-26 12:55:06 +0200

    ade display in mm:ss format

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -5,7 +5,7 @@ function Timer({minutes, seconds, handleClickStart}) {
   return (
     <div className="timer">
       <div id="timer-label" >Session</div>
-      <div id="time-left">{minutes}:{seconds}</div>
+      <div id="time-left">{minutes < 10 ? '0' : ''}{minutes}:{seconds < 10 ? '0' : ''}{seconds}</div>
       <button id="start_stop" onClick={handleClickStart} >start_stop</button>
       <button id="reset">reset</button>
     </div>
