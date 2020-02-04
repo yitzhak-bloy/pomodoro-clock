4704ed9bb3a3f2067fe08f094320600884bbb30d
yitzhak-bloy <41252020+yitzhak-bloy@users.noreply.github.com>
2020-02-04 08:34:15 +0200

    Button design

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import './Timer.css';
 
 function Timer({minutes, seconds, handleClickStart, handleClickReset, timerLabel}) {
-  return (
+  return (  
     <div className="timer">
       <div id="timer-label" >{timerLabel}</div>
       <div id="time-left">{minutes < 10 ? 0 : ''}{minutes}:{seconds < 10 ? 0 : ''}{seconds}</div>
-      <button id="start_stop" onClick={handleClickStart} >start_stop</button>
-      <button id="reset" onClick={handleClickReset}>reset</button>
+      <button className='butten1' id="start_stop" onClick={handleClickStart} >start / stop</button>
+      <button className='butten1' id="reset" onClick={handleClickReset}>reset</button>
     </div>
   );
 }
