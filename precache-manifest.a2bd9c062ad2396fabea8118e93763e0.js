493fb24f12df64a947ce59d96aac4bf804756c9a
yitzhak-bloy <41252020+yitzhak-bloy@users.noreply.github.com>
2020-02-04 22:04:44 +0200

    Updates

---
+++ precache-manifest.a2bd9c062ad2396fabea8118e93763e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "092775996a81531ca11d77089a9331b5",
+    "url": "/pomodoro-clock/index.html"
+  },
+  {
+    "revision": "55bd1da740263eaa71a6",
+    "url": "/pomodoro-clock/static/css/main.35fe5318.chunk.css"
+  },
+  {
+    "revision": "8b380f18e2d3bcad4418",
+    "url": "/pomodoro-clock/static/js/2.c9e4017d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/pomodoro-clock/static/js/2.c9e4017d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "55bd1da740263eaa71a6",
+    "url": "/pomodoro-clock/static/js/main.465f9d77.chunk.js"
+  },
+  {
+    "revision": "4cc8e959fe1d967d61ff",
+    "url": "/pomodoro-clock/static/js/runtime-main.c524c67b.js"
+  }
+]);
